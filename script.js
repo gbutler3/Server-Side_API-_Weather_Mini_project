5f291bcb2077885d64813b7f90b07016eeba73f2
grace butler <gmx.butler@gmail.com>
2021-06-10 14:55:47 -0500

    added functionallity to the search button

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log("test") //making sure it is linked correctly (take out later)
+var searchInput = $(".search");
+var submitbtn = $(".submitbtn");
+
+submitbtn.on("click", function(e) {
+    e.preventDefault();
+    if (searchInput.val() === "") {
+        alert("You must enter a city");
+        return;
+    }
+    console.log("clicked button")
+    weatherpage(searchInput.val());
+});
+
+function weatherpage (){
+    console.log ("weatherpage")
+}
