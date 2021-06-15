4ce1349c33ac8d620eefa19690c4022023643f3c
grace butler <gmx.butler@gmail.com>
2021-06-14 22:30:54 -0500

    working

--- script.js
+++ script.js
@@ -8,7 +8,6 @@ var windEl= document.querySelector(".wind");
 var humidityEl= document.querySelector(".humidity");
 var uvIndexEl= document.querySelector(".uvIndex");
 
-
 //variables for forcast
 var card1= document.getElementById("#card1")
 var forecastdate= document.querySelector(".box-date")
@@ -54,42 +53,59 @@ fetch('http://api.openweathermap.org/data/2.5/weather?q='+ searchInput.val()+
         forecastwind.innerHTML=windvalue;
         forcasthumidity.innerHTML=humidityvalue;
 
-        
-
         var skyconditions =document.querySelector("box-img");
 
         if(skyconditions==="Clouds"){
-            imgtag.attr("src", "https://img.icons8.com/color/48/000000/cloud.png")
+            imgtag.attr("src", "https://openweathermap.org/img/wn/${weatherIcon}@2x.png")
         } else if(skyconditions==="Clear"){
-            imgtag.attr("src", "https://img.icons8.com/color/48/000000/summer.png")
+            imgtag.attr("src", "https://openweathermap.org/img/wn/${weatherIcon}@2x.png")
         }else if(skyconditions==="Rain"){
-            imgtag.attr("src", "https://img.icons8.com/color/48/000000/rain.png")
+            imgtag.attr("src", "https://openweathermap.org/img/wn/${weatherIcon}@2x.png")
         }
 
 //this takes the lat and long from the one above and applies it to get the uvi for that specific city
 fetch('https://api.openweathermap.org/data/2.5/onecall?lat='+ latvalue + '&lon='+ lonvalue +'&exclude=hourly&appid=4774555034df98f9c59d9ec59b6ebf72')
-.then(response2 => response2.json())
-.then(data2 => { 
-    console.log(data2)
-    var uvivalue = 'UV Index: ' + data2['current']['uvi']; 
+.then(response => response.json())
+.then(data => { 
+    console.log(data)
+    var uvivalue = 'UV Index: ' + data['current']['uvi']; 
 
 //todo: figure out why it's not setting a color
-        if (uvivalue.data2 < 3){
-            document.getElementsByClassName("uvIndex").classlist.add("green")
-        } else if (uvivalue.data2 < 6){
-            document.getElementsByClassName("uvIndex").classlist.add("yellow")
-        } else if (uvivalue.data2 < 8){
-            document.getElementsByClassName("uvIndex").classlist.add("orange")
-        } else if (uvivalue.data2 < 10){
-            document.getElementsByClassName("uvIndex").classlist.add("red")
-        }
-        console.log(uvivalue)
+        // if (value.data < 3){
+        //     document.getElementsByClassName("uvIndex").classlist.add("green")
+        // } else if (uvivalue.data < 6){
+        //     document.getElementsByClassName("uvIndex").classlist.add("yellow")
+        // } else if (uvivalue.data < 8){
+        //     document.getElementsByClassName("uvIndex").classlist.add("orange")
+        // } else if (uvivalue.data < 10){
+        //     document.getElementsByClassName("uvIndex").classlist.add("red")}
+        // console.log(uvivalue)
 
-uvIndexEl.innerHTML = uvivalue;
+// uvIndexEl.innerHTML = uvivalue;
+
+fetch('https://api.openweathermap.org/data/2.5/forecast?q=' + searchInput.val() + '&units=imperial&appid=4774555034df98f9c59d9ec59b6ebf72')
+.then(response => response.json())
+.then(data => {console.log(data)
+
+    for (i = 0; i < data.list.length; i +=8) 
+    {var dtConvertion = new Date(parseInt(data.list[i].dt) * 1000)
+        console.log(data.list[i])
+        console.log(dtConvertion)}
 
 
-//TODO: In Application it shows only data being saved and not data2
-localStorage.setItem(cityvalue,JSON.stringify(data, data2));
+        
+        //document.getElementById(i)
+        console.log(document.getElementById(`${i}`))
+
+        temperatureEl.innerHTML = temperaturevalue;
+    
+})
+
+
+
+
+//TODO: In Application it shows only data being saved and not data
+localStorage.setItem(cityvalue,JSON.stringify(data));
 
 })
 });
